88c809cc56dbec572ff3d7135279f2d602419de5
Steven Weaver <sweaver@temple.edu>
2018-05-01 23:17:13 -0400

    updating presentation

--- hivtrace/src/presentation.js
+++ hivtrace/src/presentation.js
@@ -32,6 +32,11 @@ import {
 import createTheme from 'spectacle/lib/themes/default';
 import scott_county from './images/scott_county.png';
 import nejm from './images/nejm.png';
+import response from './images/response.png';
+import title from './images/title.png';
+import options from './images/options.png';
+import network_view from './images/network_view.png';
+import cluster_list from './images/cluster_list.png';
 
 // Require CSS
 require('normalize.css');
@@ -134,16 +139,20 @@ export default class Presentation extends React.Component {
           <Heading size={2} fit textColor="primary">
             Scott County, Indiana
           </Heading>
-					<Text textColor="primary">Pop. ~24,000</Text>
+					<Text textColor="primary">Pop. ~24,000 (~15,000 aged 18-65)</Text>
 
           <Layout>
             <Fill>
           		<Image src={scott_county} width="500px" padding="50px 30px 50px 50px" />
             </Fill>
             <Fill align="center center" marginTop="50px">
-							<Text fit="true" textColor="primary" style={{marginTop:"50"}}>181 HIV Infections</Text>
-							<Text fit="true" textColor="primary">November 2014 - November 2015</Text>
-							<Text fit="true" textColor="primary" style={{marginTop:"50"}}>Linked to Injection Drug Use</Text>
+							<Text fit="true" textColor="primary" style={{marginTop:"50"}}>November 2014 - November 2015</Text>
+							<Text fit="true" textColor="primary">181 HIV Infections</Text>
+							<List fit="true">
+								<ListItem fit="true" textColor="primary">Injection Drug Use</ListItem>
+								<ListItem fit="true" textColor="primary">57.5% Men</ListItem>
+								<ListItem fit="true" textColor="primary">34 - Median Age</ListItem>
+							</List>
           		<Image src={nejm} width="800px" padding="50px 30px 50px 50px" />
             </Fill>
           </Layout>
@@ -152,79 +161,50 @@ export default class Presentation extends React.Component {
 
 
 
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
+        <Slide transition={['zoom']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
           <Heading size={2} fit textColor="primary">
-            Key Terms
+						The Importance of Intervention
           </Heading>
+					<Image src={response} padding="50px 30px 50px 50px" />
         </Slide>
 
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9" align="flex-start center">
-
-          <Text size={6} textColor="primary" textAlign="left">
-            Key Terms
-          </Text>
-
+        <Slide transition={['zoom']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9" align="flex-start flex-start">
           <Heading size={2} fit textColor="primary">
-            What is a Transmission Network in HIV-TRACE?
+						Introducing HIV-TRACE
           </Heading>
-
-          <Text size={6} textColor="primary" textAlign="left" margin="20px 0px">
-            A network of HIV transmission wholly inferred from the sequence data that is supplied. 
-          </Text>
-
-          <Text size={6} textColor="primary" textAlign="left" margin="20px 0px">
-            It is composed of discrete <span textColor="yellow" textAlign="left">clusters</span> which in turn comprise individual <span textColor="yellow" textAlign="left">nodes.</span>
-          </Text>
-
+					<Image src={title} />
+					<List margin="10">
+						<ListItem>Infers transmission networks based on supplied HIV sequence data</ListItem>
+						<ListItem>Currently in use by the Centers for Disease Control (CDC)</ListItem>
+						<ListItem>Over 30 participating molecular HIV surveillance sites</ListItem>
+					</List>
         </Slide>
 
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
 
-          <Text size={6} textColor="primary" textAlign="left">
-            Key Terms
-          </Text>
-
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9" align="flex-start flex-start">
           <Heading size={2} fit textColor="primary">
-            What is a Node in HIV-TRACE?
+						HIV-TRACE Features
           </Heading>
-
-          <Text size={6} textColor="primary" textAlign="left" margin="20px 0px">
-            A node represents a single patient. Within HIV-TRACE, it is commonly a single representative HIV-1 pol sequence from an individual patient.
-          </Text>
-         
-          <Image src="https://upload.wikimedia.org/wikipedia/commons/c/c6/HIV-genome.png" margin="60px 40px" bgColor="white" padding="50px 30px 50px 50px" />
-
+					<Image src={options} />
+					<List margin="10">
+						<ListItem>Allows sites to append new infections to their transmission network as they are sequenced</ListItem>
+						<ListItem>Sites can conduct "stand-alone" analyses to customize parameters</ListItem>
+					</List>
         </Slide>
 
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
-
-          <Text size={6} textColor="primary" textAlign="left">
-            Key Terms
-          </Text>
+        <Slide transition={['zoom']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
           <Heading size={2} fit textColor="primary">
-            How Do We Define a Cluster?
+						HIV-TRACE Network View
           </Heading>
-
-          <List>
-            <ListItem margin="20px">Find an infected individual</ListItem>
-            <ListItem margin="20px">Identify person with genetically highly similar virus</ListItem>
-            <ListItem margin="20px">Identify partners of these partners with genetically highly similar viruses</ListItem>
-            <ListItem margin="20px">Repeat until no more links can be drawn</ListItem>
-          </List>
-
+					<Image src={network_view} />
         </Slide>
 
-
-         <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
-
-          <BlockQuote>
-            <Quote>Phylogenetic support (bootstrap, posterior, etc) is designed to separate clades, not connect nodes.</Quote>
-            <Cite>Sergei L. Kosakovsky Pond</Cite>
-          </BlockQuote>
-
-
+        <Slide transition={['zoom']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
+          <Heading size={2} fit textColor="primary">
+						HIV-TRACE Tabular Data
+          </Heading>
+					<Image src={cluster_list} />
         </Slide>
-       
 
         <Slide transition={['fade']} bgColor="secondary" textColor="primary" bgImage="http://www.sciencemag.org/sites/default/files/styles/article_main_large/public/images/SIV_1280x720.jpg?itok=EduK0pcR" bgDarken="0.9">
 
